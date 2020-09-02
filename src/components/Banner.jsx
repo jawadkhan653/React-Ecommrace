88b9c31fd3f276141a8114424fc26e7829ec30c8
Jawad Khan <52871584+jawadkhan653@users.noreply.github.com>
2020-09-02 18:34:19 +0200

    Initial Commit

---
+++ src/components/Banner.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="container">
+      <header style={{ marginTop: "70px" }}>
+        <div className="headerText">
+          <div>
+            <h1>React E-Commerce</h1>
+            <p>By Jawad</p>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Banner;
